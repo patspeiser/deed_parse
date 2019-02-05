dfc10093e1628947ff2ad00863a01d56316bfe56
patspeiser <pat@socialflow.com>
2019-02-04 19:57:35 -0500

    opendir process files

--- gis_csv_parser.pl
+++ gis_csv_parser.pl
@@ -16,23 +16,38 @@ my $schema = Hunt::Schema->connect($db);
 
 #get it ready
 my $csv  = Text::CSV->new({ sep_char => ',' });
-my $path_to_gis_csv = "./suffolkcounty.csv"; 
+my $in_dir 	= "./unparsed"; 
+my $out_dir = "./parsed";
 
-#open file
-open my $fh, '<', $path_to_gis_csv or die "no";
+#open directory
+opendir my $directory, $in_dir or die 'couldnt open dir '. $in_dir;
 
-#get header from file
-my @header = $csv->header($fh);
+while (my $file = readdir($directory)){
+	if ($file ne '.' && $file ne '..'){
+		#open file
+		my $file_to_open = $in_dir . '/' . $file;
+		open my $fh, $file_to_open or warn 'couldnt open file ' . $file;
 
-while( my $line = $csv->getline($fh) ){
-	my $result = $schema->resultset('Deed')->new({
-		tax_map_id 		=> $line->[11],
-		first_name 		=> $line->[4],
-		last_name  		=> $line->[3],
-		street     		=> $line->[5],
-		street_number 	=> $line->[8],
-		city          	=> $line->[9],
-		zip           	=> $line->[0]
-	});
-	$result->insert;
+		#get header from file
+		my @header = $csv->header($fh);
+
+		#process rest of file
+		while( my $line = $csv->getline($fh) ){
+			warn $line;
+			my $result = $schema->resultset('Deed')->new({
+				tax_map_id 		=> $line->[11],
+				first_name 		=> $line->[4],
+				last_name  		=> $line->[3],
+				street     		=> $line->[5],
+				street_number 	=> $line->[8],
+				city          	=> $line->[9],
+				zip           	=> $line->[0]
+			});
+			#create db record
+			$result->insert;
+		};
+	}
 };
+
+#close dir
+closedir($directory);
